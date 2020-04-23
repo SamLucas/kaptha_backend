a3a2fbd0666769dcea5ee472eb2825e6fc08b729
Samuel Lucas <samuellucas0603@gmail.com>
2020-04-23 17:26:38 -0300

    Mudando parametros de busca.

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -2,51 +2,53 @@ import { Request, Response } from 'express'
 import knex from '@/database/connection'
 
 const index = async (req: Request, res: Response): Promise<Response> => {
-  const { cancer, chemicals, page, limit } = req.query
+  const { dataSeach = '', page = 1, limit = 4 } = req.query
 
   let PMDCancer = []
   let PMDchemicals = []
 
-  if (cancer) {
+  const terms = dataSeach.split(', ')
+
+  if (dataSeach) {
     PMDCancer = await knex('cancerEquivalenceTerms')
       .join('indexCancers', 'id_term', '=', 'idterm_descritor')
-      .where('equivalence_term', cancer)
+      .whereIn('equivalence_term', terms)
       .select('*')
-      .first()
-      .then(data => data.pmids.split(','))
-  }
 
-  if (chemicals) {
     PMDchemicals = await knex('chemicalEquivalenceTerms')
       .join('indexPolifenols', 'id_term', '=', 'idterm_descritor')
-      .where('equivalence_term', chemicals)
+      .whereIn('equivalence_term', terms)
       .select('*')
-      .first()
-      .then(data => data.pmids.split(','))
   }
 
-  const Pmid = [...new Set([...PMDCancer, ...PMDchemicals])]
+  let Pmid: number[] = [];
+  [...PMDCancer, ...PMDchemicals].forEach(ele => {
+    const arrayAux = ele.pmids.split(',')
+    Pmid = [...new Set([...Pmid, ...arrayAux])]
+  })
 
-  const [count] = await knex('articlesTotal')
-    .whereIn('pmid', Pmid).count()
+  const offset = (page - 1) * limit
+  const pmidSearch = Pmid.slice(offset, offset + parseInt(limit))
 
-  res.header('X-Total-Count', count['count(*)'])
+  res.header('X-Total-Count', `${Pmid.length}`)
 
   await knex('articlesTotal')
-    .whereIn('pmid', Pmid)
-    .limit(limit)
-    .offset((page - 1) * limit)
-    .select('*').then(async data => {
-      const articles = await Promise.all(data.map(async ele => {
-        const response = await knex('ruleAssociationsExtracted')
-          .where('pmid_article', ele.pmid)
-          .select('*')
+    .whereIn('pmid', pmidSearch)
+    .select('*')
+    .then(async (data) => {
+      const articles = await Promise.all(
+        data.map(async (ele) => {
+          const response = await knex('ruleAssociationsExtracted')
+            .where('pmid_article', ele.pmid)
+            .select('*')
 
-        return { ...ele, ruleAssociationsExtracted: response }
-      }))
+          return { ...ele, ruleAssociationsExtracted: response }
+        })
+      )
 
-      return res.json(articles)
-    }).catch(err => {
+      return res.json({ pages: Pmid.length, articles })
+    })
+    .catch((err) => {
       return res.json({ err, message: 'Algo de errado não esta certo.' })
     })
 }
