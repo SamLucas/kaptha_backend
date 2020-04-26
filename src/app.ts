93b3f4d81a62fad4ff21d9fcb98ec71db1a9a465
Samuel Lucas <samuellucas0603@gmail.com>
2020-04-26 11:53:51 -0300

    Configurações de acesso e banco de dados

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,9 @@ class App {
 
     private middlewares (): void {
       this.express.use(express.json())
-      this.express.use(cors())
+      this.express.use(cors({
+        allowedHeaders: ['x-total-count', 'Content-Type']
+      }))
       this.express.use(morgan(`${process.env.MORGAN_TYPE}`))
     }
 
